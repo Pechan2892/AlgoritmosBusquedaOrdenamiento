81a57b6ccdbc32d81f86af92853c1494ddd039c7
crisalfredo <cristianalfredomartinezgarcia@gmail.com>
2025-05-15 20:03:45 -0600

    Working on the Class ' GeneradorNumeros'

--- src/com/proyecto/algoritmos/App.java
+++ src/com/proyecto/algoritmos/App.java
@@ -1,45 +1,8 @@
 package com.proyecto.algoritmos;
 
-import com.proyecto.algoritmos.busqueda.BusquedaBinaria;
-import com.proyecto.algoritmos.ordenamiento.MergeSort;
-import com.proyecto.algoritmos.ordenamiento.QuickSort;
-import com.proyecto.algoritmos.util.GeneradorNumeros;
-import com.proyecto.algoritmos.util.MensajeSecreto;
-
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        // Generamos el array de números aleatorios
-        GeneradorNumeros generador = new GeneradorNumeros();
-        int[] numeros = generador.generarNumeros(1000, 1000, 9999);
 
-        // Selección aleatoria de la "clave mágica"
-        int claveMagica = generador.seleccionarClave(numeros);
-        String mensajeSecreto = MensajeSecreto.asociarMensaje(claveMagica);
-
-        // Mostramos el array original y el ordenado
-        System.out.println("Array generado:");
-        generador.mostrarArray(numeros);
-
-        // Ordenamiento usando QuickSort
-        QuickSort quickSort = new QuickSort();
-        quickSort.ordenar(numeros);
-        System.out.println("\nArray ordenado con QuickSort:");
-        generador.mostrarArray(numeros);
-
-        // Búsqueda binaria para encontrar la clave mágica
-        BusquedaBinaria busqueda = new BusquedaBinaria();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\nIngresa el número mágico: ");
-        int numeroIngresado = scanner.nextInt();
-
-        boolean encontrado = busqueda.buscar(numeros, numeroIngresado);
-        if (encontrado) {
-            System.out.println("¡Correcto! " + mensajeSecreto);
-        } else {
-            System.out.println("Número incorrecto.");
-        }
     }
 }
 
--- src/com/proyecto/algoritmos/GeneradorNumeros.java
+++ src/com/proyecto/algoritmos/GeneradorNumeros.java
@@ -1,4 +1,37 @@
 package com.proyecto.algoritmos;
-
+import java.util.Random;
 public class GeneradorNumeros {
+
+    //Arreglo privado, buena encapsulación
+    private int [] numeros;
+
+
+    // Constructor: recibe la cantidad y llena el arreglo con números aleatorios
+
+    public GeneradorNumeros(int cantidad) {
+        numeros = new int[cantidad];
+        Random r = new Random();
+
+        for (int i = 0; i < cantidad; i++) {
+            numeros[i] = r.nextInt(90000) + 10000;
+        }
+
+    }
+
+    //Getter para acceder al arreglo desde fuera (buena práctica)
+
+    public int[] getNumeros() {
+        return numeros;
+    }
+
+    //Método para imprimir el arreglo, 10 por línea
+
+    public void imprimeNumeros() {
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
+            }
+        }
+    }
 }
