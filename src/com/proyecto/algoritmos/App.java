0316423e7bb75b60f1ed5d5e6dbafd01c0c3f402
crisalfredo <cristianalfredomartinezgarcia@gmail.com>
2025-05-15 20:19:54 -0600

    Working on the Class App printing the Original array: 'Working fine and genereting the 1000 numbers as requested on the excercise'

--- src/com/proyecto/algoritmos/App.java
+++ src/com/proyecto/algoritmos/App.java
@@ -3,6 +3,11 @@ package com.proyecto.algoritmos;
 public class App {
     public static void main(String[] args) {
 
+        GeneradorNumeros g = new GeneradorNumeros(1000);
+
+        System.out.println("Arreglo Original: ");
+        g.imprimeNumeros();
+
     }
 }
 
