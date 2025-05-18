f47651edc1e4f03095e89bdce4f102717cd5fa4b
Rodrigo Cristales <rodrigo10cristales@gmail.com>
2025-05-17 21:51:57 -0600

    Metodo medidor de tiempo

--- src/com/proyecto/algoritmos/MedidorTiempo.java
+++ src/com/proyecto/algoritmos/MedidorTiempo.java
@@ -1,4 +1,15 @@
 package com.proyecto.algoritmos;
 
 public class MedidorTiempo {
+
+    //tiempo de ejecucion del codigo
+
+    public long medirTiempo(Runnable algoritmo) {
+
+        long inicio = System.nanoTime();
+        algoritmo.run();
+        long fin = System.nanoTime();
+        return fin - inicio;
+
+        }
 }
